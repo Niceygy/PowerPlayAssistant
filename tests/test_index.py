e5ddb0ef3f24ea89e82093ac1454998468af8925
Ava Whale <niceygy@niceygy.net>
2025-06-05 09:05:01 +0100

    tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -22,6 +22,6 @@ def test_system_search():
         
 def test_megaships():
     with app.test_client() as client:
-        response = client.get("/404")
-        assert response.status_code == 404
-        assert response.text.__contains__('umami.track("error_404");')
+        response = client.get("/results?system=Sol&power=Jerome+Archer&taskName=Scan+Megaship+Datalinks&choice=Reinforce")
+        assert response.status_code == 200
+        assert response.text.__contains__("Found 15 megaships in Jerome")
